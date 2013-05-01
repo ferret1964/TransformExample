2941893f0cc5652262491b3cdf4bbce34c9a032c
Jerry Goodnough <ferret@stomrwoods.com>
2013-05-01 12:27:14 -0700

    Test versin 1.2 of the DocumentTransformer

---
+++ src/main/java/org/socraticgrid/transformexample/App.java
@@ -0,0 +1,36 @@
+package org.socraticgrid.transformexample;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.socraticgrid.documenttransformer.Transformer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:MDWSDocumentTransformer.xml");
+        Transformer tx = (Transformer) ctx.getBean("Xform");
+        Resource res = new ClassPathResource("PatientDataRequest_meds_10013.xml");
+       
+        String result="";
+        try
+        {
+            result = tx.transform("Medications", res.getInputStream());
+        }
+        catch (IOException ex)
+        {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println(result);
+        
+    }
+}
